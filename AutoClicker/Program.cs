7389b16bef6588265182b8509164d7bfbb7c8dad
Duc Truong <phuduc1193@gmail.com>
2019-01-20 11:39:16 -0600

    add error logging

--- AutoClicker/Program.cs
+++ AutoClicker/Program.cs
@@ -1,19 +1,31 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace AutoClicker
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
             Application.Run(MainForm.Instance);
+        }
+
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(writer, e.Exception);
+
+                File.WriteAllText("error.txt", writer.ToString());
+            }
         }
     }
 }

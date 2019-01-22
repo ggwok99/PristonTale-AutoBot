4bb791d941fc545cd5898fea4f1497b6d5c147ae
Duc Truong <phuduc1193@gmail.com>
2019-01-22 16:19:32 -0600

    resolve merge

--- AutoClicker/Strategies/PottingStrategy.cs
+++ AutoClicker/Strategies/PottingStrategy.cs
@@ -36,14 +36,6 @@ namespace AutoClicker.Strategies
             KeySTM = data.STM.Key;
         }
 
-        protected float _maxHP;
-        protected float _maxMP;
-        protected float _maxSTM;
-
-        protected const int _hpMemAddress = 0x03324D50;
-        protected const int _mpMemAddress = 0x03324D54;
-        protected const int _stmMemAddress = 0x03324D58;
-
         protected override double TimeOutInSecond()
         {
             return .1;
